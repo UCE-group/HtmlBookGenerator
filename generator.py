78ed7e9c83084ca6909ef44750a69ec05b541911
xundaoxd <13263201672@163.com>
2018-11-06 09:34:56 +0800

    add threading support

--- generator.py
+++ generator.py
@@ -3,6 +3,8 @@ import os
 import pdfkit
 import requests
 from PyPDF2 import PdfFileWriter, PdfFileReader
+import threading
+import time
 
 from URLProvider import URLProvider
 
@@ -28,23 +30,42 @@ def html_transform(text):
     return text
 
 
+class URL_resolver(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+    def run(self):
+        html = get_html(self.url)
+        html = html_transform(html)
+        print('generate html {}'.format(self.url))
+        self.pdf_name = os.path.join(TMP_DIR, self.url.split('/')[-1] + '.pdf')
+        pdfkit.from_string(html, self.pdf_name)
+        
+        
+
 def main():
     provider = URLProvider()
-    pdf_write = PdfFileWriter()
+    threads = []
     for url in provider.next():
-        html = get_html(url)
-        html = html_transform(html)
-        print('generate html {}'.format(url))
-        pdf_name = os.path.join(TMP_DIR, url.split('/')[-1] + '.pdf')
-        pdfkit.from_string(html, pdf_name)
-        pdf_reader = PdfFileReader(open(pdf_name, 'rb'))
+        thread = URL_resolver(url)
+        thread.start()
+        threads.append(thread)
+
+    pdf_write = PdfFileWriter()
+    for t in threads:
+        t.join()
+        pdf_reader = PdfFileReader(open(t.pdf_name, 'rb'))
         page_counts = pdf_reader.getNumPages()
         for i in range(page_counts):
             page = pdf_reader.getPage(i)
             pdf_write.addPage(page)
+
     pdf_write.write(open(OUTPUT_FILE, 'wb'))
     print('Done!')
 
 
 if __name__ == '__main__':
+    s = time.time()
     main()
+    e = time.time()
+    print('耗时 {}s'.format(e - s))
